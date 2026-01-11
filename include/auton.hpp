027e6c9a08bd317bc0dbeac75464b07e5271d012
James Si <james.peicheng.si+GitHub@gmail.com>
2026-01-11 00:34:54 -0700

    Added functions and variables for auton selector

--- include/auton.hpp
+++ include/auton.hpp
@@ -2,28 +2,51 @@
 #include <tuple>
 #include <vector>
 #include "pros/llemu.hpp"
+#include "pros/apix.h"
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
 
 using namespace std;
 class Auton;
 
-/*
-This will have all the variables and functions related to autonomous
-*/
+// Type alias for auton functions using 
+using AutonFunc = void(*)();
 
+// Struct for auton metadata 
+struct AutonEntry { 
+    const char* name; 
+    AutonFunc func; 
+};
+
+// Declaration of variables
 extern int screenstate;
+extern const AutonEntry autonTable[];
 extern uint8_t last_state;
 extern uint8_t state;
+extern int maxState;
 
+extern const double Field_wall;
+extern const double Sensor_offset;
+extern double a;
+
+// Function declarations 
+void runAuton(int auton); 
 void Screen_Display();
 void AutonSelector();
 
+double getAvgDistance(int milliseconds);
+void resetOrigin();
+void resetYFromWall(lemlib::Chassis& chassis);
+void resetXFromWall(lemlib::Chassis& chassis);
+
+//auton routines
 void testAuton();
 void rightRed();
 void leftRed(); 
 void rightBlue();
 void leftBlue();
 void skills();
-void runSelectedAuton();
 
+// intake functions
 void Intake(int velocity);
 void Center_Intake(int velocity);
